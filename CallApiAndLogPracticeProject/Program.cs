289e8440ea8b975ce5a5d8dd5704e4977b509d50
Nick Hester <nick.d.hester@accruent.com>
2020-04-20 21:37:57 -0500

    initial commit

---
+++ CallApiAndLogPracticeProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+
+namespace CallApiAndLogPracticeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = args[0];
+            string outputFilePath = args[1];
+            int numberToSubmit;
+
+            // read file
+            StreamReader reader = new StreamReader(inputFilePath);
+            string inputFileText = reader.ReadToEnd();
+            reader.Close();
+            if (!int.TryParse(inputFileText, out numberToSubmit))
+            {
+                throw new Exception($"Number could not be read from file at {inputFilePath}");
+            }
+
+            // call API
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://numbersapi.com");
+
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync($"/{numberToSubmit}").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("API failed to return a response");
+            }
+
+            var responseText = response.Content.ReadAsStringAsync().Result;
+
+            // write file
+            StreamWriter writer = new StreamWriter(outputFilePath);
+            writer.WriteLine(responseText);
+            writer.Close();
+
+            Console.WriteLine("Success");
+        }
+    }
+}
